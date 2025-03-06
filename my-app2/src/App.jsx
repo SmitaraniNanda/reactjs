d7a8b9d93861ca5aa4af7d6078594b09669d7ff5
Smita <smitan@questk2.com>
2025-03-06 16:54:19 +0530

    do

--- my-app2/src/App.jsx
+++ my-app2/src/App.jsx
@@ -2,5 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Ref from './Ref.jsx'; 
+import Memo from './Move.jsx';
+import Callback from './Callback.jsx';
+
+function App() {
+   return ( <>
+      <Ref/>
+      <Memo/>
+      <Callback/>
+          </>
+  )
+}
 
 export default App
--- my-app2/src/main.jsx
+++ my-app2/src/main.jsx
@@ -2,15 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Ref from './Ref.jsx'; 
-import Memo from './Move.jsx';
-import Callback from './Callback.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    <Ref/>
-     <Memo/>
-     <Callback/>
   </StrictMode>,
 )
