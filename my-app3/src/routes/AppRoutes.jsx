aa3bb33c3c1094a210f4ddab87e56540505f259d
Smita <smitan@questk2.com>
2025-03-07 17:03:39 +0530

    do

--- my-app3/src/routes/AppRoutes.jsx
+++ my-app3/src/routes/AppRoutes.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from "../components/Home";
 import About from "../components/About";
 import Contact from "../components/Contact";
